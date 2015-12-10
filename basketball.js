e3d1bdcb81c642f308b3347dff52c4bac5ec4675
nickianimeforever24 <niseekah.hamilton@gmail.com>
2015-12-10 22:10:00 +0000

    Basketball

--- basketball.js
+++ basketball.js
@@ -1,3 +1,9 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
+    if (name.length>15) {
+        alert("Stop, your name is too long")
+    }
+    
+    return name;
+
 };
